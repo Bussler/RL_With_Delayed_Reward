52512de4e7a390e45cd603e20d40eef3bdbf3fea
Bussler <maarten.bussler@gmail.com>
2025-06-22 22:43:57 +0200

    [fix](RustSimulation): Fix main.rs to use the rust code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use drone_environment::DroneEnvironment;
-use drone_environment::Vec3;
 
 fn main() {
     println!("Initializing Drone Environment...");
 	let mut env = DroneEnvironment::new(
-        Vec3::new(0.0, 0.0, 0.0),
+        (0.0, 0.0, 0.0),
         10.0,
         0.1,
         10.0,
         Some(1.0),
     );
 
-    env.add_target(0, Vec3::new(10.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 0.0), None, None);
+    env.add_target(0, (10.0, 0.0, 0.0), (0.0, 0.0, 0.0), None, None);
     
+    let obs1 = env.reset(None);
+    println!("Observation 1: {:?}", obs1);
+
+    let obs2 = env.step((1.0, 0.0, 0.0));
+    println!("Observation 2: {:?}", obs2);
 }
